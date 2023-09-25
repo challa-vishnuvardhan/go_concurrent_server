4071506d26eda1c205cfbeb7687ec0837415fb96
Vishnu Challa <vishnuchalla666@gamil.com>
2023-09-25 12:02:41 +0530

    go routine

--- db/concurrent_db.go
+++ db/concurrent_db.go
@@ -1,19 +1,26 @@
 package db
 
 import (
+	"fmt"
 	"go_concurrent_server/resource"
 	"sync"
 )
 
-func ServiceClient(input resource.Request) (resource.Response, error) {
+func ServiceClient(input resource.Request, wg *sync.WaitGroup, data1 chan string) {
+	defer wg.Done()
 	var clientsMutex sync.Mutex
 	clientsMutex.Lock()
-	output := "HI" + " " + input.Message
+	output := "Hi" + " " + input.Message
 	clientsMutex.Unlock()
+	fmt.Println("1", output)
+	data1 <- output
+}
 
-	var result resource.Response
-
-	result.Message = output
-	return result, nil
-
+func ServiceClientResponse(wg *sync.WaitGroup, data2 chan string) {
+	defer wg.Done()
+	var clientsMutex sync.Mutex
+	clientsMutex.Lock()
+	output := " How are you ?"
+	clientsMutex.Unlock()
+	data2 <- output
 }
--- service/concurrent_service.go
+++ service/concurrent_service.go
@@ -3,15 +3,27 @@ package service
 import (
 	"go_concurrent_server/db"
 	"go_concurrent_server/resource"
+	"sync"
 )
 
 func ServiceClient(input resource.Request) (resource.Response, error) {
-	result, err := db.ServiceClient(input)
-	if err != nil {
-		return result, err
+	var result resource.Response
+	var wg sync.WaitGroup
+	data1 := make(chan string)
+	data2 := make(chan string)
 
-	}
+	wg.Add(2)
 
+	go func() {
+		db.ServiceClient(input, &wg, data1)
+	}()
+
+	go func() {
+		db.ServiceClientResponse(&wg, data2)
+	}()
+
+	result.Message = <-data1 + <-data2
+	wg.Wait()
+	// Wait for both goroutines to finish
 	return result, nil
-
 }
